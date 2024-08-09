73bf56bf26eaba7619ba78933994bff4a2364cd7
xides <alcides.torres1@gmail.com>
2024-08-08 18:36:30 -0600

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import playformCompress from "@playform/compress";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), playformCompress()]
+});
